3ce343721d3136526bd128d972d5d9d0021dcc43
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-02-21 23:57:52 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# yarana-bot --- やらなボット
+
+Line Bot Application
+
+## Feature
+
+やると褒めます
+
+やらないと怒ります
+
+The bot
+
+Praises you if you do
+
+Give you a scolding if you don't
---
+++ server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	app, err := NewYarana(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+		os.Getenv("APP_BASE_URL"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/callback", app.Callback)
+	// TODO: Support HTTPS by using `ListenAdnServeTLS`, reverse proxy or etc.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Yarana app
+type Yarana struct {
+	bot        *linebot.Client
+	appBaseURL string
+}
+
+// NewYarana creates Yarana
+func NewYarana(channelSecret, channelToken, appBaseURL string) (*Yarana, error) {
+	apiEndpointBase := os.Getenv("ENDPOINT_BASE")
+	if apiEndpointBase == "" {
+		apiEndpointBase = linebot.APIEndpointBase
+	}
+	bot, err := linebot.New(
+		channelSecret,
+		channelToken,
+		linebot.WithEndpointBase(apiEndpointBase), // Usually you omit this.
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &Yarana{
+		bot:        bot,
+		appBaseURL: appBaseURL,
+	}, nil
+}
+
+// Callback
+func (app *Yarana) Callback(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello user")
+}
