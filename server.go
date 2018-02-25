9bd913bfd5225087df5f850e2da6e6108bbf2487
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-02-25 21:16:30 +0900

    Add Callback method

--- server.go
+++ server.go
@@ -52,7 +52,79 @@ func NewYarana(channelSecret, channelToken, appBaseURL string) (*Yarana, error) 
 	}, nil
 }
 
-// Callback is callback of http request
+// Callback function for http server
 func (app *Yarana) Callback(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello user")
+	events, err := app.bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		log.Printf("Got event %v", event)
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if err := app.handleText(message, event.ReplyToken, event.Source); err != nil {
+					log.Print(err)
+				}
+			case *linebot.ImageMessage:
+				if err := app.handleImage(message, event.ReplyToken); err != nil {
+					log.Print(err)
+				}
+			default:
+				log.Printf("Unknown message: %v", message)
+			}
+		case linebot.EventTypeFollow:
+			if err := app.replyText(event.ReplyToken, "Got followed event"); err != nil {
+				log.Print(err)
+			}
+		case linebot.EventTypeUnfollow:
+			log.Printf("Unfollowed this bot: %v", event)
+		case linebot.EventTypeJoin:
+			if err := app.replyText(event.ReplyToken, "Joined "+string(event.Source.Type)); err != nil {
+				log.Print(err)
+			}
+		case linebot.EventTypeLeave:
+			log.Printf("Left: %v", event)
+		case linebot.EventTypePostback:
+			data := event.Postback.Data
+			if data == "DATE" || data == "TIME" || data == "DATETIME" {
+				data += fmt.Sprintf("(%v)", *event.Postback.Params)
+			}
+			if err := app.replyText(event.ReplyToken, "Got postback: "+data); err != nil {
+				log.Print(err)
+			}
+		case linebot.EventTypeBeacon:
+			if err := app.replyText(event.ReplyToken, "Got beacon: "+event.Beacon.Hwid); err != nil {
+				log.Print(err)
+			}
+		default:
+			log.Printf("Unknown event: %v", event)
+		}
+	}
+}
+
+func (app *Yarana) replyText(replyToken, text string) error {
+	if _, err := app.bot.ReplyMessage(
+		replyToken,
+		linebot.NewTextMessage(text),
+	).Do(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (app *Yarana) handleText(message *linebot.TextMessage, replyToken string, source *linebot.EventSource) error {
+	switch message.Text {
+	}
+	return nil
+}
+
+func (app *Yarana) handleImage(message *linebot.ImageMessage, replyToken string) error {
+	return nil
 }
