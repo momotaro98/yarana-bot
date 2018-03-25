42d2e9e294407e7122bcffbbff10d130361c5cd4
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-03-25 16:50:41 +0900

    test: Add unit tests on server_test.go and create Makefile

--- server_test.go
+++ server_test.go
@@ -1,26 +1,31 @@
 package main
 
-import "time"
+import (
+	"testing"
+	"time"
+)
 
-// YaranaDataCallMock is a test struct of DataCall for Yarana-bot
-type YaranaDataCallMock struct {
+const linebotAPIError401Str = "linebot: APIError 401 Authentication failed due to the following reason: invalid token. Confirm that the access token in the authorization header is valid."
+
+// StandardYaranaDataCallMock is a test struct of DataCall for Yarana-bot
+type StandardYaranaDataCallMock struct {
 }
 
-// NewYaranaDataCallMock is a constructor of YaranaDataCallMock
-func NewYaranaDataCallMock() (*YaranaDataCallMock, error) {
-	return &YaranaDataCallMock{}, nil
+// NewStandardYaranaDataCallMock is a constructor of StandardYaranaDataCallMock
+func NewStandardYaranaDataCallMock() (*StandardYaranaDataCallMock, error) {
+	return &StandardYaranaDataCallMock{}, nil
 }
 
 // GetKotosByUserID is a method of DataCall interface
-func (c *YaranaDataCallMock) GetKotosByUserID(userID string) ([]*KotoData, error) {
+func (c *StandardYaranaDataCallMock) GetKotosByUserID(userID string) ([]*KotoData, error) {
 	// Get Koto by userID from something
 	id := "0123456789a"
-	title := "Test Title"
+	title := "TestTitle"
 	koto, err := NewKotoData(id, userID, title)
 	if err != nil {
 		return nil, err
 	}
-	title2 := "Test Title 2"
+	title2 := "TestTitle2"
 	koto2, err := NewKotoData(id, userID, title2)
 	if err != nil {
 		return nil, err
@@ -29,12 +34,12 @@ func (c *YaranaDataCallMock) GetKotosByUserID(userID string) ([]*KotoData, error
 }
 
 // AddKoto is a method of DataCall interface
-func (c *YaranaDataCallMock) AddKoto(koto *KotoData) error {
+func (c *StandardYaranaDataCallMock) AddKoto(koto *KotoData) error {
 	return nil
 }
 
 // GetActivitiesByKotoDataID is a method of DataCall interface
-func (c *YaranaDataCallMock) GetActivitiesByKotoDataID(kotoID string) ([]*ActivityData, error) {
+func (c *StandardYaranaDataCallMock) GetActivitiesByKotoDataID(kotoID string) ([]*ActivityData, error) {
 	loc, _ := time.LoadLocation("Asia/Tokyo")
 	timeStamp := time.Date(2018, 3, 3, 3, 3, 35, 0, loc)
 	activity, err := NewActivityData("0123456789a", kotoID, timeStamp)
@@ -51,6 +56,82 @@ func (c *YaranaDataCallMock) GetActivitiesByKotoDataID(kotoID string) ([]*Activi
 }
 
 // AddActivity is a method of DataCall interface
-func (c *YaranaDataCallMock) AddActivity(activity *ActivityData) error {
+func (c *StandardYaranaDataCallMock) AddActivity(activity *ActivityData) error {
 	return nil
+}
+
+func TestProcessGetKotosStandard(t *testing.T) {
+	dataCall, _ := NewStandardYaranaDataCallMock()
+	app, _ := NewYarana(
+		"TEST_YARANA_API_BASE_URL",
+		"TEST_YARANA_API_ADDKOTO_KEY",
+		"TEST_YARANA_API_ADDACTIVITY_KEY",
+		dataCall,
+	)
+
+	err := app.processGetKotos("replyToken", "userID", "TestTitle")
+	if err != nil {
+		if err.Error() != linebotAPIError401Str {
+			t.Fatal("err should be linebot APIError 401", " Got error: ", err)
+		}
+	} else {
+		t.Fatal("err should be linebot APIError 401")
+	}
+}
+
+func TestProcessAddKotosStandard(t *testing.T) {
+	dataCall, _ := NewStandardYaranaDataCallMock()
+	app, _ := NewYarana(
+		"TEST_YARANA_API_BASE_URL",
+		"TEST_YARANA_API_ADDKOTO_KEY",
+		"TEST_YARANA_API_ADDACTIVITY_KEY",
+		dataCall,
+	)
+
+	err := app.processAddKoto("replyToken", "userID", "TestTitleAddKoto")
+	if err != nil {
+		if err.Error() != linebotAPIError401Str {
+			t.Fatal("err should be linebot APIError 401", " Got error: ", err)
+		}
+	} else {
+		t.Fatal("err should be linebot APIError 401")
+	}
+}
+
+func TestProcessGetActivitiesStandard(t *testing.T) {
+	dataCall, _ := NewStandardYaranaDataCallMock()
+	app, _ := NewYarana(
+		"TEST_YARANA_API_BASE_URL",
+		"TEST_YARANA_API_ADDKOTO_KEY",
+		"TEST_YARANA_API_ADDACTIVITY_KEY",
+		dataCall,
+	)
+
+	err := app.processGetActivities("replyToken", "userID", "")
+	if err != nil {
+		if err.Error() != linebotAPIError401Str {
+			t.Fatal("err should be linebot APIError 401", " Got error: ", err)
+		}
+	} else {
+		t.Fatal("err should be linebot APIError 401")
+	}
+}
+
+func TestProcessAddActivityStandard(t *testing.T) {
+	dataCall, _ := NewStandardYaranaDataCallMock()
+	app, _ := NewYarana(
+		"TEST_YARANA_API_BASE_URL",
+		"TEST_YARANA_API_ADDKOTO_KEY",
+		"TEST_YARANA_API_ADDACTIVITY_KEY",
+		dataCall,
+	)
+
+	err := app.processAddActivity("replyToken", "userID", "TestTitle")
+	if err != nil {
+		if err.Error() != linebotAPIError401Str {
+			t.Fatal("err should be linebot APIError 401", " Got error: ", err)
+		}
+	} else {
+		t.Fatal("err should be linebot APIError 401")
+	}
 }
