26900f73490fe182812bc70b224020d34384645e
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-03-29 00:53:17 +0900

    enhance: Add AnalyzeInputTextInJapanese func in userrequest.go

--- userrequest.go
+++ userrequest.go
@@ -35,8 +35,10 @@ func (r *UserTextRequest) AnalyzeInputText(text string) error {
 		return fmt.Errorf("Can't analyze: %s", text)
 	}
 
-	err := r.AnalyzeInputTextInEnglish(text)
-	if err == nil && r.Type != "" {
+	if err := r.AnalyzeInputTextInEnglish(text); r.Type != "" && err == nil {
+		return nil
+	}
+	if err := r.AnalyzeInputTextInJapanese(text); r.Type != "" && err == nil {
 		return nil
 	}
 
@@ -72,5 +74,80 @@ func (r *UserTextRequest) AnalyzeInputTextInEnglish(text string) error {
 	default:
 		return fmt.Errorf("Can't analyze: %s", text)
 	}
+	return nil
+}
+
+// AnalyzeInputTextInJapanese analyzes input text from user with Japanese
+func (r *UserTextRequest) AnalyzeInputTextInJapanese(text string) error {
+	words := strings.Fields(text)
+	if len(words) < 1 {
+		return fmt.Errorf("Can't analyze: %s", text)
+	}
+	// Japanese mode doesn't accept multi "words"
+	if len(words) > 1 {
+		return fmt.Errorf("Can't analyze: %s", text)
+	}
+
+	if text == "やること教えて" || text == "やることを教えて" {
+		r.Type = RequstTypeGetKotos
+		return nil
+	}
+
+	if n := len(text) - 3*5; n >= 0 {
+		if text[n:] == "を登録して" {
+			if len(text[:n]) <= 0 {
+				return fmt.Errorf("Can't analyze: %s", text)
+			}
+			r.Type = RequstTypeAddKoto
+			r.VariableKeyword = text[:n]
+			return nil
+		}
+	}
+
+	if text == "履歴教えて" || text == "履歴を教えて" {
+		r.Type = RequstTypeGetActivities
+		return nil
+	} else if n := len(text) - 3*7; n >= 0 {
+		if text[n:] == "の履歴を教えて" {
+			if len(text[:n]) <= 0 {
+				return fmt.Errorf("Can't analyze: %s", text)
+			}
+			r.Type = RequstTypeGetActivities
+			r.VariableKeyword = text[:n]
+			return nil
+		}
+	}
+
+	if n := len(text) - 3*5; n >= 0 {
+		if text[n:] == "をやったよ" {
+			if len(text[:n]) <= 0 {
+				return fmt.Errorf("Can't analyze: %s", text)
+			}
+			r.Type = RequstTypeAddActivity
+			r.VariableKeyword = text[:n]
+			return nil
+		}
+	}
+	if n := len(text) - 3*4; n >= 0 {
+		if text[n:] == "をやった" || text[n:] == "やったよ" {
+			if len(text[:n]) <= 0 {
+				return fmt.Errorf("Can't analyze: %s", text)
+			}
+			r.Type = RequstTypeAddActivity
+			r.VariableKeyword = text[:n]
+			return nil
+		}
+	}
+	if n := len(text) - 3*3; n >= 0 {
+		if text[n:] == "やった" {
+			if len(text[:n]) <= 0 {
+				return fmt.Errorf("Can't analyze: %s", text)
+			}
+			r.Type = RequstTypeAddActivity
+			r.VariableKeyword = text[:n]
+			return nil
+		}
+	}
+
 	return nil
 }
